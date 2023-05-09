075b8c0ab32f7532741c572370819e273cef5464
astapov <dmytroastapow@gmail.com>
2023-05-09 11:23:57 +0300

    add project

---
+++ online_store_app/forms.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from django.utils import timezone
+
+from online_store_app.models import User, Product, Purchase, Return
+
+
+class UserForm(UserCreationForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+
+    class Meta:
+        model = User
+        fields = ("username", "wallet",)
+
+    def save(self, commit=True):
+        user = super(UserForm, self).save(commit=False)
+        user.set_password(self.cleaned_data.get("password"))
+        if commit:
+            user.save()
+
+        return user
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'img', 'stock_availability']
+
+
+class PurchaseForm(ModelForm):
+    class Meta:
+        model = Purchase
+        fields = ['amount']
+
+    def clean_amount(self):
+        pass
+
+
+class ReturnForm(ModelForm):
+    class Meta:
+        model = Return
+        fields = ['purchase']
--- online_store_app/views.py
+++ online_store_app/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LogoutView, LoginView
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models import QuerySet
+from django.views.generic import CreateView, UpdateView, ListView, DetailView
 
-# Create your views here.
+from online_store_app.forms import UserForm, ProductForm, PurchaseForm
+from online_store_app.models import Product, Purchase, Return
+
+
+class BaseView(ListView):
+    model = Product
+    paginate_by = 4
+    template_name = 'home_page.html'
+    queryset = Product.objects.all()
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    fields = ['name', 'description', 'price', 'stock_availability']
+    template_name = 'update_product.html'
+    queryset = Product.objects.all()
+    success_url = '/'
+    login_url = 'login.html'
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    form_class = ProductForm
+    template_name = 'create_product.html'
+    success_url = '/'
+    login_url = 'login.html'
+
+
+class ProductDetailView(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = 'detail_product.html'
+    extra_context = {'form': PurchaseForm()}
+    login_url = 'login.html'
+
+
+class ProductBuyCreate(CreateView):
+    http_method_names = ['post']
+    form_class = PurchaseForm
+    success_url = '/'
+
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.user = self.request.user
+        obj.product = Product.objects.get(pk=self.kwargs.get(self.pk_url_kwarg))
+        obj.save()
+        return super().form_valid(form=form)
+
+
+class BasketDetailView(LoginRequiredMixin, ListView):
+    model = Purchase
+    template_name = 'basket.html'
+    queryset = Purchase.objects.all()
+    login_url = 'login.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+
+
+class SuperUserReturns(LoginRequiredMixin, ListView):
+    model = Return
+    template_name = 'admin_returns.html'
+    queryset = Return.objects.all()
+    login_url = 'login.html'
+
+
+class Login(LoginView):
+    http_method_names = ['get', 'post']
+    template_name = 'login.html'
+    success_url = '/'
+
+
+class Logout(LoginRequiredMixin, LogoutView):
+    next_page = '/'
+    login_url = 'login/'
+
+
+class Register(CreateView):
+    form_class = UserForm
+    template_name = 'register.html'
+    success_url = '/'
